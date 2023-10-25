9c13ae901290318e0b2c8dc88403dafaea033876
Umberto Fontanazza <umbertofontanazza@MacBook-Pro-di-Umberto.local>
2023-10-25 22:14:03 +0200

    Add annotations, defined starting heuristic

--- setcover/state.py
+++ setcover/state.py
@@ -1,22 +1,24 @@
 from __future__ import annotations
 from dataclasses import dataclass
-from numpy import array, logical_or
+from numpy import array, logical_or, bool_
+from numpy.typing import NDArray
 from functools import reduce, cache
 
 @dataclass(frozen=True)
 class State():
     current_cost: int
-    taken : tuple[array[bool]]
-    not_taken : tuple[array[bool]]
+    taken : tuple[NDArray[bool_]]
+    not_taken : tuple[NDArray[bool_]]
 
     # read docs first but @cache is a good idea
-    def __current_coverage(self) -> array[bool]:
+    def __current_coverage(self) -> NDArray[bool_]:
         return reduce(logical_or, self.taken)
 
     def is_solution(self) -> bool:
         return all(self.__current_coverage())
 
-    def take_covering_set(self, covering_set: array[bool]) -> State:
+    def take_covering_set(self, covering_set: NDArray[bool_]) -> State:
+        new_state_take : tuple[NDArray[bool_]] = (*self.taken, covering_set)
         return State(
             current_cost = self.current_cost + 1,
             taken = (*self.taken, covering_set),
@@ -24,8 +26,7 @@ class State():
         )
 
     def estimated_additional_cost(self) -> int:
-        # TODO: to be implemented
-        raise NotImplementedError()
+        return 1 # doesn't help to discard some branches but it's admissible
 
     def estimated_total_cost(self) -> int:
         return self.current_cost + self.estimated_additional_cost()
