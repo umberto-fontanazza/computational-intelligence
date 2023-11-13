18ba36efe01174baa3c1c2e1335a8fa68c3579f5
Umberto Fontanazza <umbertofontanazza@MacBook-Pro-di-Umberto.local>
2023-11-13 17:22:45 +0100

    Add test case with expert system playing second

--- labs/test/test_strategy.py
+++ labs/test/test_strategy.py
@@ -1,6 +1,12 @@
 from src.strategy import Strategy
 from src.game import Game
 
+def test_gabriele_vs_expert_system():
+    player = Strategy.gabriele()
+    opponent = Strategy.expert_system()
+    testgame = Game(4, player, opponent, True)
+    assert testgame.player_wins() == True
+
 def test_exepert_system_vs_gabriele():
     player = Strategy.expert_system()
     opponent = Strategy.gabriele()
