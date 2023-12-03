f95b116b10800d073fc0616cedc7ce654367e429
Umberto Fontanazza <umbertofontanazza@MacBook-Pro-di-Umberto.local>
2023-12-03 16:03:08 +0100

    Rename mutate  -> simple_mutate Rename mutate2 -> mutate Add docstring to combine_masked method Remove reference_random field from Population class Add Population.random factory method

--- labs/lab3/src/genome.py
+++ labs/lab3/src/genome.py
@@ -14,6 +14,8 @@ class Genome():
         return Genome(tuple(gene for gene in choices([0, 1], k = length)), fitness_fn)
 
     def combine_masked(self, other: Genome, mask: list[bool]) -> Genome:
+        """Self.genes marked by positive values of mask are applied to a copy of other"""
+
         child_genome = [gene if mask[i] else other.genes[i] for i, gene in enumerate(self.genes)]
         return Genome(tuple(child_genome), self.fitness_fn)
 
@@ -39,16 +41,16 @@ class Genome():
             child_genes = self.genes[:cut_index_one] + other.genes[cut_index_one:cut_index_two] + self.genes[cut_index_two:]
             return Genome(tuple(child_genes), self.fitness_fn)
 
-    def chromosome_fitness_gain(self, mask: list[bool], reference_random_genome: Genome) -> float:
-        """The idea is to associate a fitness value to a group of genes (chromosome), even though we can evaluate fitenss
-        of complete genomes only. The chromosome is identified in the genome with a mask (like subnet masks for ip networks).
-        To compute the fitness relative to a chromosome.
-        Likely requires one extra fitness call"""
+    # def chromosome_fitness_gain(self, mask: list[bool], reference_random_genome: Genome) -> float:
+        # """The idea is to associate a fitness value to a group of genes (chromosome), even though we can evaluate fitenss
+        # of complete genomes only. The chromosome is identified in the genome with a mask (like subnet masks for ip networks).
+        # To compute the fitness relative to a chromosome.
+        # Likely requires one extra fitness call"""
 
-        random_with_chromosome = self.combine_masked(reference_random_genome, mask)
-        return random_with_chromosome.fitness - reference_random_genome.fitness
+        # random_with_chromosome = self.combine_masked(reference_random_genome, mask)
+        # return random_with_chromosome.fitness - reference_random_genome.fitness
 
-    def chromosome_fitness_gain2(self, mask: list[bool], random_genomes: list[Genome]) -> float:
+    def chromosome_fitness_gain(self, mask: list[bool], random_genomes: list[Genome]) -> float:
         """Measure the gain of applying the chromosome identified by the mask to a pool of random genomes"""
 
         previous_average_fitness = sum([genome.fitness for genome in random_genomes]) / len(random_genomes)
@@ -56,15 +58,14 @@ class Genome():
         updated_average_fitness = sum([genome.fitness for genome in random_with_chromosome]) / len(random_with_chromosome)
         return updated_average_fitness - previous_average_fitness
 
-    def mutate(self) -> Genome:
+    def simple_mutate(self) -> Genome:
         length = len(self.genes)
         changing_index = randint(0, length - 1)
         child_genes = tuple(gene ^ 1 if i == changing_index else gene for i, gene in enumerate(self.genes))
         return Genome(child_genes, self.fitness_fn)
 
-    def mutate2(self) -> Genome:
-        """Exploit solvability of the problem, we know max fitness is 1 and minimum fitness is 0
-        Mutate a quantity of genes(loci) proportional to the distance from perfect fitness"""
+    def mutate(self) -> Genome:
+        """Swap a number of genes(loci) proportional to the distance from perfect fitness(=1)"""
 
         best_fitness_distance = 1 - self.fitness
         genome_length = len(self.genes)
@@ -82,7 +83,7 @@ class Genome():
         fittest: Genome = self
         non_improving_steps = 0
         for _ in range(max_steps):
-            children = [self.mutate2() for _ in range(λ)]
+            children = [self.mutate() for _ in range(λ)]
             previous_fittest = fittest
             fittest = max([fittest, *children], key = lambda genome: genome.fitness)
             if fittest.fitness == previous_fittest.fitness:
